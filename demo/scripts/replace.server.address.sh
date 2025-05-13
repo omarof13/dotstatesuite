e98ff4487a60c98ed7d0344a149c0d0d82277e3a
Aleksei Arhipov <arhipov.aleksei+oecd@gmail.com>
2025-05-13 10:14:07 +0300

    added kong as api-gateway example + new service that is used as demo-release sdmx json endpoint, https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-core-getdata/-/issues/39 https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-core-getdata/-/issues/16, https://gitlab.com/sis-cc/.stat-suite/dotstatsuite-core-getdata/-/issues/17

--- demo/scripts/replace.server.address.sh
+++ demo/scripts/replace.server.address.sh
@@ -5,7 +5,7 @@ echo "Replace host to ... : "$2
 echo "Replace host from . : "$3
 echo "JSON files detected :"
 find $1 -type f -name "tenants.json"
-find $1 -type f -name "datasources.json"
+find $1 -type f -name "settings.json"
 
 find $1 -type f -name "settings.json" -exec sed -Ei 's#"http://localhost:3004/api"|"http://'$3':3004/api"#"http://'$2':3004/api"#g' {} +
 
@@ -17,8 +17,6 @@ find $1 -type f -name "settings.json" -exec sed -Ei 's#"http://localhost:3005"|"
 
 find $1 -type f -name "settings.json" -exec sed -Ei 's#"http://'$3':7001"#"http://'$2':7001"#g' {} +
 
-find $1 -type f -name "tenants.json" -exec sed -Ei 's#"http://'$3:'#"http://'$2:'#g' {} +
-find $1 -type f -name "tenants.json" -exec sed -Ei 's#"http://'$3/'#"http://'$2/'#g' {} +
+find $1 -type f -name "tenants.json" -exec sed -Ei 's#"http://'$3'#"http://'$2'#g' {} +
 
 echo "JSON configuration files updated with the following host address: "$2
-
